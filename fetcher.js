c04cc8886bccaa3f5b4b4bcefb4be1ec729f9f93
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-11-11 02:06:48 +0000

    Page Downloader

--- fetcher.js
+++ fetcher.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const fs = require('fs');
 
-request('http://example.edu/', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
+let args = process.argv[2]
+let path = process.argv[3]
+request(args, function (error, response, body) {
+  if (error) {
+  console.error('error:', error); // Print the error if one occurred
+  return;
+  }
+  if() {
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  return;
+  }
   console.log('body:', body); // Print the HTML for the Google homepage.
+  fs.writeFile(path, body, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${path}`)
+  })
 });
